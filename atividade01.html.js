bde1563fd63b5a409063ab9c4369d0f06dc9f529
Daviniel Ribeiro <davinielribe3@gmail.com>
2026-05-22 18:28:20 -0300

    mantem as alteraçõe:w!s do input color ao atualizar

--- atividade01.html.js
+++ atividade01.html.js
@@ -1,37 +1,35 @@
-
 const elementoForm = document.getElementById("formulario");
 
-elementoForm.addEventListener("submit",(e) => e.preventDefault());
+elementoForm.addEventListener("submit", (e) => e.preventDefault());
 
 const seletorCor = document.getElementById("corFavorita");
 
-seletorCor.addEventListener("input", (e) =>{
+seletorCor.addEventListener("input", (e) => {
     let corNova = e.target.value;
+
     localStorage.setItem("primaryColor", corNova);
 
     document.documentElement.style.setProperty("--primary-color", corNova);
 });
 
-document.addEventListener("DOMContentLoaded", () =>
-{
-    if (localStorage.getItem("primaryColor")) {
+document.addEventListener("DOMContentLoaded", () => {
 
-        seletorCor.value = localStorage.getItem("primaryColor");
+    const corSalva = localStorage.getItem("primaryColor");
 
-        document.documentElement.style.setProperty("--primary-color", seletorCor.value);
+    if (corSalva) {
+
+        seletorCor.value = corSalva;
+
+        document.documentElement.style.setProperty("--primary-color", corSalva);
 
     } else {
 
         const rootStyles = window.getComputedStyle(document.documentElement);
 
-        const primaryColor = rootStyles.getPropertyValue("--primary-color").trim();
+        const primaryColor = rootStyles
+            .getPropertyValue("--primary-color")
+            .trim();
 
         seletorCor.value = primaryColor;
     }
-
-    const rootStyles = window.getComputedStyle(document.documentElement);
-
-    const prymaryColor = rootStyles.getPropertyValue("--primary-color").trim();
-
-    seletorCor.value = prymaryColor;
-})
+});
