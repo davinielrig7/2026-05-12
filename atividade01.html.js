40cfe58c1ce2ad68c6dc993ddb3a7b092a4bd51b
Daviniel Ribeiro <davinielribe3@gmail.com>
2026-05-21 21:17:32 -0300

    mantem alterações do input color ao atualizar
    a pagina

--- atividade01.html.js
+++ atividade01.html.js
@@ -1,3 +1,4 @@
+
 const elementoForm = document.getElementById("formulario");
 
 elementoForm.addEventListener("submit",(e) => e.preventDefault());
@@ -6,12 +7,28 @@ const seletorCor = document.getElementById("corFavorita");
 
 seletorCor.addEventListener("input", (e) =>{
     let corNova = e.target.value;
+    localStorage.setItem("primaryColor", corNova);
 
     document.documentElement.style.setProperty("--primary-color", corNova);
 });
 
 document.addEventListener("DOMContentLoaded", () =>
 {
+    if (localStorage.getItem("primaryColor")) {
+
+        seletorCor.value = localStorage.getItem("primaryColor");
+
+        document.documentElement.style.setProperty("--primary-color", seletorCor.value);
+
+    } else {
+
+        const rootStyles = window.getComputedStyle(document.documentElement);
+
+        const primaryColor = rootStyles.getPropertyValue("--primary-color").trim();
+
+        seletorCor.value = primaryColor;
+    }
+
     const rootStyles = window.getComputedStyle(document.documentElement);
 
     const prymaryColor = rootStyles.getPropertyValue("--primary-color").trim();
